6cd1928d0fc935e6337eb54d579d33eef9e38cc5
Kahiro-M <0204.kahiro@gmail.com>
2026-03-30 14:45:29 +0900

    pyinstallerによるビルド対応

---
+++ make.bat
@@ -0,0 +1,1 @@
+pyinstaller --onefile --icon=icon.ico msg2eml.py -n msg2eml
--- msg2eml.py
+++ msg2eml.py
@@ -1,3 +1,4 @@
+import sys
 import os
 import glob
 import extract_msg
@@ -7,9 +8,14 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 
+if getattr(sys, 'frozen', False):
+    # PyInstallerで実行中
+    baseDir = Path(sys.executable).parent
+else:
+    # 通常のPythonスクリプト実行中
+    baseDir = Path(__file__).resolve().parent
 
-scriptDir = Path(__file__).resolve().parent
-os.chdir(scriptDir)
+os.chdir(baseDir)
 
 print('====== MSG to EML Converter ======')
 print('                           v.1.0.0')
@@ -22,7 +28,7 @@ if not extract_msg:
 
 # 変換元のMSGファイルが格納されている"input_msg"フォルダを確認
 if not os.path.exists("input_msg"):
-    print('Error: "input_msg"フォルダが見つかりません。')
+    print('"input_msg"フォルダが見つかりません。')
     print('変換元のMSGファイルが格納されている"input_msg"フォルダを指定してください。')
     inputDir = input('フォルダパス: ')
     TMP_IN_DIR = Path(inputDir.strip().strip("'\"")).resolve()
@@ -30,9 +36,14 @@ else:
     TMP_IN_DIR = Path("input_msg")
 
 IN_DIR = TMP_IN_DIR.resolve()
-OUT_DIR = Path("output_eml")
+OUT_DIR = Path("output_eml").resolve()
 OUT_DIR.mkdir(parents=True, exist_ok=True)
 
+print('------ 変換元/出力先 ------')
+print(f"Input Directory: {IN_DIR}")
+print(f"Output Directory: {OUT_DIR}")
+
+print('------ 変換開始 ------')
 for msg_path in glob.glob(str(IN_DIR / "*.msg")):
     name = Path(msg_path).stem
     emlPath = OUT_DIR / f"{name}.eml"
@@ -74,4 +85,8 @@ for msg_path in glob.glob(str(IN_DIR / "*.msg")):
     #  EMLファイル書き出し
     emlPath.write_bytes(mime.as_bytes())
 
-    print(f"Converted: {msg_path} -> {emlPath}")
+    print(f"  Converted:")
+    print(f"    {msg_path}")
+    print(f"      -> {emlPath}")
+
+print('------ 変換完了 ------')
